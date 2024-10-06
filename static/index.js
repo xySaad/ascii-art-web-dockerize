d64099cade4fb36ddd5e8997a88f9cbb6930b3eb
srm <saadrm.dev@gmail.com>
2024-10-06 17:51:04 +0100

    [Frontend] Add banners selection

--- static/index.js
+++ static/index.js
@@ -2,9 +2,12 @@ const convertButton = document.querySelector("#convertButton");
 const ascii = document.querySelector(".ascii");
 convertButton.addEventListener("click", async () => {
   const inputField = document.querySelector("#inputField");
+  const banners = document.querySelector("#banners");
   const res = await fetch(window.location.origin + "/api/ascii", {
     method: "POST",
-    body: `input=${encodeURIComponent(inputField.value)}&banner=standard`,
+    body: `input=${encodeURIComponent(inputField.value)}&banner=${
+      banners.value
+    }`,
   });
   console.log(ascii);
   ascii.textContent = await res.text();
