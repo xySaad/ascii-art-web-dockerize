0069fdcce3e0a9cee6b32942549303eeca0e9123
srm <saadrm.dev@gmail.com>
2024-10-06 10:06:51 +0100

    Dispense JavaScript

--- static/index.js
+++
@@ -1,9 +0,0 @@
-const darkIcon = document.querySelector("#theme-switcher span#dark");
-
-darkIcon.addEventListener("click", () => {
-  if (document.body.id == "dark") {
-    document.body.id = "white";
-  } else {
-    document.body.id = "dark";
-  }
-});
